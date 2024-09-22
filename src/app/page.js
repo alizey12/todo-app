cd34ccce37c24b34e36422b1ebf178bed3764d74
alizey121 <alizey121@gmail.com>
2024-09-22 16:12:01 +0500

    todo

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Navbar from "../navbar/nav";
+export default function Home() {
+  return (
+   <>
+<Navbar />
+    <h1 className="bg-black">hello world</h1>
+   </> 
+    
+  );
+}
---
+++ src/navbar/nav.js
@@ -0,0 +1,11 @@
+
+
+export default function Navbar() {
+  return (
+   <>
+
+    <h1 className="bg-pink-500">hello world</h1>
+   </> 
+    
+  );
+}
